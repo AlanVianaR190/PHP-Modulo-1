ff28e467b9d8fee3fce378124ca88709629b68ba
AlanVianaR190 <alanvr.unip.edfis@gmail.com>
2024-04-09 15:03:11 -0300

    add

---
+++ ex000/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Me livrando da Maldição</title>
+</head>
+<body>
+    <h1>
+        <?php 
+            //no php e usado echo para saida de dados, tambem pode ser usado o print
+            echo "Olá, mundo... ";
+            print "Olá mundo, dnovo 🌏";
+        ?>
+    </h1>
+    <p>Vamos tentar nos livrar da maldição</p>
+</body>
+</html>
